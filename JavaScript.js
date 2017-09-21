3037adde775097f98561efde41d2808ca860b60a
Girdland <markus.girdland@atollic.com>
2017-09-21 16:31:18 +0200

    Website
    Version 1.0

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+﻿function makeQuote() {
+    var quotes = ["I have relieved", "Let me ask you helping", "I'm happy to teach me", "My understanding have become clearly at all"];
+
+    var random = Math.floor((Math.random() * 4) + 0);
+
+    document.getElementById("quote").innerHTML = quotes[random];
+}
